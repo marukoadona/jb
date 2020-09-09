83a1f774dc9be90bf9cd8082be12820655371155
marukoadona <70728758+marukoadona@users.noreply.github.com>
2020-09-09 19:47:09 +0900

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,19 @@
+function PopUp(hideOrshow) {
+        if (hideOrshow == 'hide') {
+            document.getElementById('ac-wrapper').style.display = "none";
+        }
+        else  if(localStorage.getItem("popupWasShown") == null) {
+            localStorage.setItem("popupWasShown",1);
+            document.getElementById('ac-wrapper').removeAttribute('style');
+        }
+    }
+    window.onload = function () {
+        setTimeout(function () {
+            PopUp('show');
+        }, 0);
+    }
+
+
+    function hideNow(e) {
+        if (e.target.id == 'ac-wrapper') document.getElementById('ac-wrapper').style.display = 'none';
+    }
