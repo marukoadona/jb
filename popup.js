2b1c526c08c9fc2ab3c5fdf6c524585c64ba7627
marukoadona <70728758+marukoadona@users.noreply.github.com>
2020-09-09 19:07:35 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
+localStorage.getItem('key');
+localStorage.setItem('key','value');
+
 $j(document).ready(function() {
-    $j("#popup").delay(2000).fadeIn();
-    $j('#popup-close').click(function(e) // You are clicking the close button
+    if(localStorage.getItem('popState') != 'shown'){
+        $j('#popup').delay(2000).fadeIn();
+        localStorage.setItem('popState','shown')
+    }
+
+    $j('#popup-close, #popup').click(function() // You are clicking the close button
     {
-    $j('#popup').fadeOut(); // Now the pop up is hiden.
-    });
-    $j('#popup').click(function(e)
-    {
-    $j('#popup').fadeOut();
+        $j('#popup').fadeOut(); // Now the pop up is hidden.
     });
 });
 
-<script type="text/javascript" src="scripts/jquery.cookies.2.2.0.min.js"></script> 
+<script type="text/javascript" src="scripts/jquery.cookies.2.2.0.min.js"></script>
