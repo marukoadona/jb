7f6cb0176167e29ffb9ba32f5e9049322f7866d6
marukoadona <70728758+marukoadona@users.noreply.github.com>
2020-09-15 14:16:08 +0900

    Add files via upload

--- exp_jailbreak.js
+++ exp_jailbreak.js
@@ -26408,16 +26408,16 @@ var _ = malloc_nogc.pop();
 if (main_ret == 179 || main_ret == 0) {
 window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Exploit Loaded ✔</h1>";
 
-if (ExploitMira != null){
+/*if (ExploitMira != null){
 	if (ExploitMira == "HEN"){window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Exploit Loaded ✔ Now Loading HEN ...</h1>";}
-	/*else {
+	else {
 	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Exploit Loaded ✔ Now Loading Mira ...</h1>";}
     setTimeout(function(){
     var script = document.createElement('script');script.src = MiraSRC;document.getElementsByTagName('head')[0].appendChild(script);
     var script = document.createElement('script');script.src = "exp_loader.js";document.getElementsByTagName('head')[0].appendChild(script);
   },1000);*/
   }
-}
+
 else {
   window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'>Jailbreak failed! - Reboot your PS4 and try again.</h1>";
 }
